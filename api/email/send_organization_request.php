351c4766eadd0d4cd31eb05ed9d9a7184f35325b
zhenya <zalixdan@mail.ru>
2025-09-10 23:50:36 +0500

    emailsend

--- api/email/send_organization_request.php
+++ api/email/send_organization_request.php
@@ -1,9 +1,15 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST , OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// Обработка preflight OPTIONS запроса
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 // Подключаем PHPMailer и конфигурацию
 require_once '../vendor/autoload.php';
